c48553f0447884fe92633f82356cab8ac9e791a9
neil <mallarineil9@gmail.com>
2023-06-15 03:36:38 -0400

    Setup dev environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/react-ts-sass-jest-rtl-vite-stater/",
+  base: "/react-pomodoro-timer/",
   plugins: [react()],
 });
